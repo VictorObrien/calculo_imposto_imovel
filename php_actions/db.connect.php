be3d04da5c0972d71a76c550b63f421e51fc6706
PAULO VICTOR REBOUÇAS PEREIRA <victorobrien@hotmail.com>
2020-02-10 15:38:47 -0300

    Update db.connect.php

--- php_actions/db.connect.php
+++ php_actions/db.connect.php
@@ -1,6 +1,6 @@
 <?php
 // Conexão com banco de dados
-$servername = "postgres://iqfbarjinwukva:47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08@ec2-52-203-160-194.compute-1.amazonaws.com:5432/d9l9ga4bsm2319";
+$servername = "pec2-52-203-160-194.compute-1.amazonaws.com;
 $username = "iqfbarjinwukva";
 $password = "47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08";
 $db_name = "iqfbarjinwukva";
