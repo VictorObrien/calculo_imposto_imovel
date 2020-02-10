3b0a8acd67a201198f3fd6592f678fc13bf32887
PAULO VICTOR REBOUÇAS PEREIRA <victorobrien@hotmail.com>
2020-02-10 15:24:25 -0300

    Update db.connect.php

--- php_actions/db.connect.php
+++ php_actions/db.connect.php
@@ -1,9 +1,9 @@
 <?php
 // Conexão com banco de dados
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db_name = "calculoimposto";
+$servername = "ec2-52-203-160-194.compute-1.amazonaws.com";
+$username = "iqfbarjinwukva";
+$password = "47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08";
+$db_name = "iqfbarjinwukva";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
 mysqli_set_charset($connect, "utf8");
