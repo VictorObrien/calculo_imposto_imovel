75c42dbaf4941de83a3ee4e28a29dd27fe27a03f
PAULO VICTOR REBOUÇAS PEREIRA <victorobrien@hotmail.com>
2020-02-19 10:33:55 -0300

    Update db.connect.php

--- php_actions/db.connect.php
+++ php_actions/db.connect.php
@@ -1,11 +1,12 @@
 <?php
 // Conexão com banco de dados
-$servername = "postgres://iqfbarjinwukva:47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08@ec2-52-203-160-194.compute-1.amazonaws.com:5432/d9l9ga4bsm2319";
+$servername = "ec2-52-203-160-194.compute-1.amazonaws.com";
 $username = "iqfbarjinwukva";
 $password = "47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08";
 $db_name = "d9l9ga4bsm2319";
+$port = "5432";
 
-$connect = mysqli_connect($servername, $username, $password, $db_name);
+$connect = mysqli_connect($servername, $username, $password, $db_name, $port);
 mysqli_set_charset($connect, "utf8");
 
 if(mysqli_connect_error()):
