c32a228428b704677972328b879fb0d2e5098ff9
PAULO VICTOR REBOUÇAS PEREIRA <victorobrien@hotmail.com>
2020-02-19 10:41:09 -0300

    Update db.connect.php

--- php_actions/db.connect.php
+++ php_actions/db.connect.php
@@ -4,9 +4,8 @@ $servername = "ec2-52-203-160-194.compute-1.amazonaws.com";
 $username = "iqfbarjinwukva";
 $password = "47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08";
 $db_name = "d9l9ga4bsm2319";
-$port = "5432";
 
-$connect = mysqli_connect($servername, $username, $password, $db_name, $port);
+$connect = mysqli_connect($servername, $username, $password, $db_name);
 mysqli_set_charset($connect, "utf8");
 
 if(mysqli_connect_error()):
