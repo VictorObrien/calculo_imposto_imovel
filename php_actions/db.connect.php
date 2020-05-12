1cc094e26453b88c5897960a989392f830b032e8
PAULO VICTOR REBOUÇAS PEREIRA <victorobrien@hotmail.com>
2020-05-11 22:08:52 -0300

    adicionando porta de conexão

--- php_actions/db.connect.php
+++ php_actions/db.connect.php
@@ -4,8 +4,9 @@ $servername = "ec2-52-203-160-194.compute-1.amazonaws.com";
 $username = "iqfbarjinwukva";
 $password = "47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08";
 $db_name = "d9l9ga4bsm2319";
+$porta = "5432";
 
-$connect = mysqli_connect($servername, $username, $password, $db_name);
+$connect = mysqli_connect($servername, $username, $password, $db_name, $porta);
 mysqli_set_charset($connect, "utf8");
 
 if(mysqli_connect_error()):
