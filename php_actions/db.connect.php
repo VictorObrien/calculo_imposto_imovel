d4704676706d402caf61c658dad27d219470ceeb
PAULO VICTOR REBOUÇAS PEREIRA <victorobrien@hotmail.com>
2020-05-11 22:25:32 -0300

    testando função pg_connect

--- php_actions/db.connect.php
+++ php_actions/db.connect.php
@@ -1,15 +1,17 @@
 <?php
 // Conexão com banco de dados
-$servername = "ec2-52-203-160-194.compute-1.amazonaws.com";
+/*$servername = "ec2-52-203-160-194.compute-1.amazonaws.com";
 $username = "iqfbarjinwukva";
 $password = "47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08";
 $db_name = "d9l9ga4bsm2319";
-$porta = "5432";
+$porta = "5432";*/
 
-$connect = mysqli_connect($servername, $username, $password, $db_name, $porta);
-mysqli_set_charset($connect, "utf8");
+$con_string = "host=ec2-52-203-160-194.compute-1.amazonaws.com port=5432 dbname=d9l9ga4bsm2319 user=iqfbarjinwukva password=47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08"
 
-if(mysqli_connect_error()):
+$connect = pg_connect($con_string);
+//mysqli_set_charset($connect, "utf8");
+
+/*if(mysqli_connect_error()):
 	echo "Erro na conexão: ".mysqli_connect_error();
 endif;
-?>
+*/
