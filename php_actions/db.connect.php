8666c7926107bc5304e43dca8a305bdb7e88ee2b
PAULO VICTOR REBOUÇAS PEREIRA <victorobrien@hotmail.com>
2020-05-25 11:35:04 -0300

    Update conexão postgres heroku

--- php_actions/db.connect.php
+++ php_actions/db.connect.php
@@ -6,12 +6,21 @@ $password = "47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08";
 $db_name = "d9l9ga4bsm2319";
 $porta = "5432";*/
 
-$con_string = "host=ec2-52-203-160-194.compute-1.amazonaws.com port=5432 dbname=d9l9ga4bsm2319 user=iqfbarjinwukva password=47b048b0384b913df2be789ad1836c80125cc716ac1243c915aef8cafe2cee08"
-
-$connect = pg_connect($con_string);
 //mysqli_set_charset($connect, "utf8");
 
 /*if(mysqli_connect_error()):
 	echo "Erro na conexão: ".mysqli_connect_error();
-endif;
-*/
+endif;*/
+
+$servidor = "ec2-54-175-117-212.compute-1.amazonaws.com";
+     $porta = 5432;
+     $bancoDeDados = "deudsfv7ifp6hc";
+     $usuario = "cdfntnfeyhajih";
+     $senha = "fdc803b41a29d228c44b2aecbc667bb7da0a39e2ea395db2e1f80fe9c3cae37d";
+
+$conexao = pg_connect("host=$servidor port=$porta dbname=$bancoDeDados user=$usuario password=$senha");
+
+if(!$conexao) {
+	die("Não foi possível se conectar ao banco de dados.");
+}
+?>
